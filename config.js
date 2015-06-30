e04960209597397685f85e80ea58687a1c90ce35
stasundr <stasundr@yandex.ru>
2015-06-30 11:28:16 -0400

    Update

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 4000,
     directory: __dirname,
     sessionSecret: 'GKSLDJNVMXCNVPSJGSPOEGJPFKXLC23652873$*&(#@*&%&@#',
-    muscle: __dirname + '/bin/muscle3.8.31_i86darwin64',
+    muscle: __dirname + '/bin/muscle3.8.31_i86linux64',
     mail: 'lhmg.ohg@yandex.ru',
     mailPassword: 'dfjdl%q-3gdsVFalks_3-',
     mailOptions: {
         from: 'lhmg.ohg@yandex.ru',
         subject: 'ohg. Your mtDNA analysis results are ready!',
-        text: 'Here we go! :)\n'
+        text: ''
     },
 
     redisPort: process.env.REDIS_PORT || 6379,
